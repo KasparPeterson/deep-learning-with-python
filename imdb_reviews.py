fca11f23f60ccc9b47dfc0cb83a838888cc7c207
KasparPeterson <kasparpeterson@hotmail.com>
2018-03-10 23:08:08 +0200

    Imdb reviews - fits model

--- imdb_reviews.py
+++ imdb_reviews.py
@@ -1,4 +1,6 @@
 from keras.datasets import imdb
+from keras import models
+from keras import layers
 import numpy as np
 
 
@@ -15,11 +17,35 @@ def vectorize_sequences(sequences, dimension=10000):
 	return results
 
 
-(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+def get_model():
+	model = models.Sequential()
+	model.add(layers.Dense(16, activation="relu", input_shape=(10000,)))
+	model.add(layers.Dense(16, activation="relu"))
+	model.add(layers.Dense(1, activation="sigmoid"))
+	model.compile(optimizer="rmsprop", loss="binary_crossentropy", metrics=['accuracy'])
+	return model
 
-x_train = vectorize_sequences(train_data)
-x_test = vectorize_sequences(test_data)
 
-y_train = np.asarray(train_labels).astype("float32")
-y_test = np.asarray(test_labels)
+def get_data():
+	(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+	x_train = vectorize_sequences(train_data)
+	x_test = vectorize_sequences(test_data)
+	y_train = np.asarray(train_labels).astype("float32")
+	y_test = np.asarray(test_labels).astype("float32")
+	return x_train[:10000], x_train[10000:], y_train[:10000], y_train[10000:]
+
+
+x_val, x_train, y_val, y_train = get_data()
+
+model = get_model()
+history = model.fit(
+	x_train,
+	y_train,
+	epochs=20,
+	batch_size=512,
+	validation_data=[x_val, y_val])
+
+history_dict = history.history
+print("Dict: ", str(history_dict.keys()))
+
 
