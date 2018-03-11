94e633309314084268d4895a9e85405ff381598b
KasparPeterson <kasparpeterson@hotmail.com>
2018-03-11 15:32:37 +0200

    Imdb reviews - draws plot and predicts

--- imdb_reviews.py
+++ imdb_reviews.py
@@ -2,12 +2,39 @@ from keras.datasets import imdb
 from keras import models
 from keras import layers
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+EPOCHS = 20
+
+
+def sentence_to_vector(sentence):
+	sequence = encode(sentence)
+	sequence = preprocess_sequence(sequence)
+	return vectorize_sequences([sequence])
+
+
+def preprocess_sequence(sequence):
+	result = []
+	for s in sequence:
+		if s is not None:
+			result.append(s)
+	return result
 
 
 def decode(sequence):
 	word_index = imdb.get_word_index()
 	reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 	return " ".join(reverse_word_index.get(i - 3, "?") for i in sequence)
+
+
+def encode(sentence):
+	word_index = imdb.get_word_index()
+	sequence = []
+	for word in sentence.split():
+		value = word_index.get(word.lower()) + 3
+		sequence.append(value)
+	return sequence
 
 
 def vectorize_sequences(sequences, dimension=10000):
@@ -32,20 +59,65 @@ def get_data():
 	x_test = vectorize_sequences(test_data)
 	y_train = np.asarray(train_labels).astype("float32")
 	y_test = np.asarray(test_labels).astype("float32")
-	return x_train[:10000], x_train[10000:], y_train[:10000], y_train[10000:]
+	return x_train, x_test, y_train, y_test
 
 
-x_val, x_train, y_val, y_train = get_data()
+def plot_losses(history_dict):
+	loss_values = history_dict["loss"]
+	val_loss_values = history_dict["val_loss"]
+
+	epochs = range(1, EPOCHS + 1)
+
+	plt.plot(epochs, loss_values, 'bo', label="Training loss")
+	plt.plot(epochs, val_loss_values, "b", label="Validation loss")
+	plt.title("Training and validation loss")
+	plt.xlabel("Epochs")
+	plt.ylabel("Loss")
+	plt.legend()
+	plt.show()
+
+
+def plot_accuracies(history_dict):
+	acc_values = history_dict["acc"]
+	val_acc_values = history_dict["val_acc"]
+
+	epochs = range(1, EPOCHS + 1)
+
+	plt.plot(epochs, acc_values, "bo", label="Training acc")
+	plt.plot(epochs, val_acc_values, "b", label="Validation acc")
+	plt.title("Training and validation accuracy")
+	plt.xlabel("Epochs")
+	plt.ylabel("Loss")
+	plt.legend()
+	plt.show()
+
+x_train, x_test, y_train, y_test = get_data()
+
+x_val = x_train[:10000]
+x_partial = x_train[10000:]
+y_val = y_train[:10000]
+y_partial = y_train[10000:]
 
 model = get_model()
 history = model.fit(
-	x_train,
-	y_train,
-	epochs=20,
+	x_partial,
+	y_partial,
+	epochs=EPOCHS,
 	batch_size=512,
 	validation_data=[x_val, y_val])
 
-history_dict = history.history
-print("Dict: ", str(history_dict.keys()))
+results = model.evaluate(x_test, y_test)
+print("results: ", results)
 
+history_dict = history.history
+plot_losses(history_dict)
+plt.clf()
+plot_accuracies(history_dict)
+
+sentence = "Wow amazing stuff"
+vector = sentence_to_vector(sentence)
+
+predicted = model.predict([vector])
+print("Predicted:")
+print(predicted)
 
