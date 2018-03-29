948032fcf615085baaa4beb609229f94ffef59a0
KasparPeterson <kasparpeterson@hotmail.com>
2018-03-29 09:04:29 +0300

    Adds trained model evaluation example

--- embedding-from-scratch/data_loader.py
+++ embedding-from-scratch/data_loader.py
@@ -2,6 +2,7 @@ import os
 
 imdb_dir = "aclImdb"
 train_dir = os.path.join(imdb_dir, "train")
+test_dir = os.path.join(imdb_dir, "test")
 
 
 def load_data():
@@ -16,6 +17,25 @@ def load_data():
                 texts.append(f.read())
                 f.close()
 
+                if label_type == "neg":
+                    labels.append(0)
+                else:
+                    labels.append(1)
+
+    return texts, labels
+
+
+def load_test_data():
+    labels = []
+    texts = []
+
+    for label_type in ["neg", "pos"]:
+        dir_name = os.path.join(test_dir, label_type)
+        for fname in sorted(os.listdir(dir_name)):
+            if fname[-4:] == ".txt":
+                f = open(os.path.join(dir_name, fname))
+                texts.append(f.read())
+                f.close()
                 if label_type == "neg":
                     labels.append(0)
                 else:
